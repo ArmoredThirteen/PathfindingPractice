080b832db2a659858339753be2d22b4b93edfe46
ArmoredThirteen <6456070+ArmoredThirteen@users.noreply.github.com>
2020-04-06 17:46:37 -0700

    Got breadth first to work with some basic (and currently a bit broken...) feedback over time. Fixed marking nodes as dirty when modifying the terrain since they were not serializing

---
+++ ProjectPathPractice/Assets/_ATE/Scripts/Pathing/BreadthFirst/PathBuilder.cs
@@ -0,0 +1,68 @@
+﻿using ATE.Terrain;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ATE.Pathing.BreadthFirst
+{
+    public class PathBuilder
+	{
+        public bool complete;
+
+        public TerrainNode from;
+        public TerrainNode to;
+
+        public List<TerrainNode> visited;
+        public Queue<TerrainNode> frontier;
+
+        public List<TerrainNode> finalPath;
+
+
+        public void RestartPathing(TerrainNode fromNode, TerrainNode toNode)
+        {
+            complete = false;
+
+            from = fromNode;
+            to = toNode;
+
+            visited = new List<TerrainNode> ();
+            frontier = new Queue<TerrainNode> ();
+            finalPath = new List<TerrainNode> ();
+
+            visited.Add (from);
+            frontier.Enqueue (from);
+        }
+        
+        // Returns true when pathing is complete
+        public void IteratePath()
+        {
+            Debug.Log ("Iterate");
+
+            if (frontier.Count <= 0)
+            {
+                Debug.Log ("Frontier is empty");
+                complete = true;
+                return;
+            }
+            
+            TerrainNode currNode = frontier.Dequeue ();
+            if (currNode == to)
+            {
+                Debug.Log ($"Found: {currNode.name}");
+                complete = true;
+                return;
+            }
+
+            for (int i = 0; i < currNode.paths.Count; i++)
+            {
+                TerrainNode path = currNode.paths[i];
+                if (path == null || path.type == TerrainType.Blocked || visited.Contains (path))
+                    continue;
+
+                visited.Add (path);
+                frontier.Enqueue (path);
+            }
+        }
+		
+	}
+}
---
+++ ProjectPathPractice/Assets/_ATE/Scripts/Pathing/BreadthFirst/RunBreadthFirst.cs
@@ -0,0 +1,52 @@
+﻿using ATE.Pathing.BreadthFirst;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ATE.Pathing
+{
+	public class RunBreadthFirst : MonoBehaviour
+	{
+        public GameObject visitedObj;
+        public GameObject frontierObj;
+        public GameObject pathObj;
+
+        public PathTarget from;
+        public PathTarget to;
+
+
+        private List<GameObject> feedbackObjs;
+        private PathBuilder builder;
+        
+
+        private void Start()
+        {
+            feedbackObjs = new List<GameObject> ();
+            builder = new PathBuilder ();
+            builder.RestartPathing (from.currNode, to.currNode);
+        }
+
+        private void Update()
+        {
+            if (builder.complete)
+                return;
+
+            //while (!builder.complete)
+                builder.IteratePath ();
+            
+            // Rebuild all feedback objects rather hackily
+            while (feedbackObjs.Count > 0)
+            {
+                Destroy (feedbackObjs[0]);
+                feedbackObjs.RemoveAt (0);
+            }
+            // Instantiate visited feedback objects
+            for (int i = 0; i < builder.visited.Count; i++)
+                GameObject.Instantiate (visitedObj, builder.visited[i].transform.position, transform.rotation, transform);
+            // Instantiate frontier feedback objects
+            for (int i = 0; i < builder.frontier.Count; i++)
+                GameObject.Instantiate (frontierObj, builder.frontier.ToArray()[i].transform.position, transform.rotation, transform);
+        }
+
+    }
+}
